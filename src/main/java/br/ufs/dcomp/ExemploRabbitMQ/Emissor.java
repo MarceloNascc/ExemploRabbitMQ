86cde33faa405d1795b6b65b775f7d7670fb086c
MarceloNascc <marcelo.nasccc@gmail.com>
2023-02-09 14:41:52 +0000

    feat: send 50 messages to the queue

--- src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Emissor.java
+++ src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Emissor.java
@@ -3,6 +3,7 @@ package br.ufs.dcomp.ExemploRabbitMQ;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import java.util.concurrent.TimeUnit;
 
 public class Emissor {
 
@@ -10,20 +11,30 @@ public class Emissor {
 
   public static void main(String[] argv) throws Exception {
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost("ip-da-instancia-da-aws"); // Alterar
-    factory.setUsername("usuário-do-rabbitmq-server"); // Alterar
-    factory.setPassword("senha-do-rabbitmq-server"); // Alterar
-    factory.setVirtualHost("/");    Connection connection = factory.newConnection();
+    factory.setHost("100.27.10.131"); // Alterar
+    factory.setUsername("admin"); // Alterar
+    factory.setPassword("rabbitmq2022.2"); // Alterar
+    factory.setVirtualHost("/");
+    Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
 
                       //(queue-name, durable, exclusive, auto-delete, params); 
     channel.queueDeclare(QUEUE_NAME, false,   false,     false,       null);
 
-    String message = "Olá!!!";
+    Integer messageNumber = 0;
     
-                    //  (exchange, routingKey, props, message-body             ); 
-    channel.basicPublish("",       QUEUE_NAME, null,  message.getBytes("UTF-8"));
-    System.out.println(" [x] Mensagem enviada: '" + message + "'");
+    while(true){
+      String message = "TESTE 0 - my server " + messageNumber++;
+      
+                            //  (exchange, routingKey, props, message-body             ); 
+      channel.basicPublish("",       QUEUE_NAME, null,  message.getBytes("UTF-8"));
+      System.out.println(" [x] Mensagem enviada: '" + message + "'");
+      TimeUnit.SECONDS.sleep(4);
+      
+      if(messageNumber == 50){
+        break;
+      }
+    }
 
     channel.close();
     connection.close();
